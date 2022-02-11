25c8bb106619a3a8fcca59f5500abc3f351a5157
AvgEnjoyer <yariktkachenko83@gmail.com>
2022-02-11 21:02:13 +0200

    output

--- Output.h
+++ Output.h
@@ -2,3 +2,13 @@
 #include<stdio.h>
 #include"Company.h"
 #include"Structures.h"
+
+void printCompany(Company company)
+{
+	printf("Company's name: %s\n", company.name);
+	printf("Company's status: %d\n", company.status);
+}
+void printEmployee(Employee employee)
+{
+	printf("Employee's name: %s\n", employee.name);
+}
