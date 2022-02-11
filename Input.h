45ebe7396cd711cf587550dd1093e83e975c605f
AvgEnjoyer <yariktkachenko83@gmail.com>
2022-02-11 20:57:48 +0200

    input

--- Input.h
+++ Input.h
@@ -3,3 +3,24 @@
 #include<string.h>
 #include"Structures.h"
 #include"Company.h"
+
+void readCompany(Company* company)
+{
+	char name[20];
+	int status;
+	name[0] = '\0';
+	printf("Enter company\'s name: ");
+	scanf("%s", name);
+
+	strcpy(company->name,name);
+	printf("Enter company\'s status: ");
+	scanf("%d", status);
+	company->status = status;
+}
+void readEmployee(Employee* employee)
+{
+	char name[20];
+	printf("Enter employee\'s name: ");
+		scanf("%s", name);
+	strcpy(employee->name, name);
+}
